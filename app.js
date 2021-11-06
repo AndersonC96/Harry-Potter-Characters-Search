6b04bada02b94d0508210a289aadf68f445bec04
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-06 19:37:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let hpCharacters = [];
 searchBar.addEventListener('keyup', (e) => {
     const searchString = e.target.value.toLowerCase();
     const filteredCharacters = hpCharacters.filter((character) => {
-        return (
+        return(
             character.name.toLowerCase().includes(searchString) ||
             character.house.toLowerCase().includes(searchString)
         );
@@ -12,11 +12,11 @@ searchBar.addEventListener('keyup', (e) => {
     displayCharacters(filteredCharacters);
 });
 const loadCharacters = async () => {
-    try {
+    try{
         const res = await fetch('https://hp-api.herokuapp.com/api/characters');
         hpCharacters = await res.json();
         displayCharacters(hpCharacters);
-    } catch (err) {
+    }catch (err){
         console.error(err);
     }
 };
@@ -25,15 +25,13 @@ const displayCharacters = (characters) => {
         .map((character) => {
             return `
             <li class="character">
-                <h2>${character.name}</h2>
+                <h2>${character.name} | ${character.actor}</h2>
                 <p><b>House: </b>${character.house}</p>
-                <p><b>Species: </b>${character.species}</p>
-                <p><b>Gender: </b>${character.gender}</p>
+                <p><b>Species: </b>${character.species} | <b>Gender: </b>${character.gender}</p>
                 <p><b>Date of birth: </b>${character.dateOfBirth}</p>
                 <p><b>Ancestry: </b>${character.ancestry}</p>
-                <p><b>Eye colour: </b>${character.eyeColour}</p>
-                <p><b>Hair colour: </b>${character.hairColour}</p>
-                <p><b>Wand: </b>Wood (${character.wand.wood}) / Core (${character.wand.core}) / Lenght (${character.wand.length})</p>
+                <p><b>Hair colour: </b>${character.hairColour} | <b>Eye colour: </b>${character.eyeColour}</p>
+                <p><b>Wand: </b><i><u>Wood</u></i> (${character.wand.wood}) | <i><u>Core</u></i> (${character.wand.core}) | <i><u>Lenght</u></i> (${character.wand.length})</p>
                 <p><b>Patronus: </b>${character.patronus}</p>
                 <img src="${character.image}"></img>
             </li>
